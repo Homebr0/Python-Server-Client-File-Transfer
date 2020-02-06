90c431994be4ed31b37e84054d730aa1766831f0
Jason Davila <jsndavila1@gmail.com>
2020-02-06 17:41:57 +0000

    Initialized input parameters for client

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-
 import sys
+import socket
+
+HOST = sys.argv[1]  # The server's hostname or IP address
+PORT = int(sys.argv[2])  # The port used by the server
+FILENAME = sys.argv[3]
 
 if __name__ == '__main__':
-    sys.stderr.write("client is not implemented yet\n")
+    sys.stderr.write("sike")
