554936e64603ffe10f6edb0ab26808b8481c84cd
Jason Davila <jsndavila1@gmail.com>
2020-02-08 00:43:05 +0000

    public tests : THREADING IMPLEMENTED

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import socket
 import sys
 import os
+from _thread import *
+import threading 
 
 HOST = "localhost"
 PORT = int(sys.argv[1])  # The port used by the server
@@ -10,12 +12,30 @@ MIB = 13107200
 fileName = ''
 count = 0
 
+print_lock = threading.Lock()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+def sockThread(conn):
+    while True:
+
+        data = conn.recv(MIB)
+
+        with open(fileName, 'wb') as f:
+            f.write(data)
+            data = conn.recv(MIB)
+
+            if not data:
+                conn.send(b'End')
+                print_lock.release()
+                break
+            
+    conn.close()
+
+
+
 if __name__ == '__main__':
     
-
 
     if (PORT <= 1023):
         sys.stderr.write("ERROR: Invalid port number\n")
@@ -30,28 +50,20 @@ if __name__ == '__main__':
             print('Waiting for incoming connections')
             conn, addr = sock.accept()
             
+            print_lock.acquire()
             count = count + 1
             print('Connected to ', addr)
 
             if (not os.path.exists(FILEPATH)):
                 os.makedirs(FILEPATH)
             fileName = FILEPATH + '/' + str(count) + '.file'
-            data = conn.recv(MIB)
-
-
-            with open(fileName, 'wb') as f:
-                f.write(data)
-                data = conn.recv(MIB)
-
-                if not data:
-                    conn.send(b'End')
-                    conn.settimeout(3)
-                    conn.close()
+            
+            start_new_thread(sockThread, (conn,))
 
         except socket.timeout:
             sys.stderr.write('ERROR: Socket timeout\n')
             exit(1)
 
-    conn.close()
     sock.close()
     
+
